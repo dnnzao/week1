e8233a932d5ed7817cf11c179c5b51e3cad8ab7d
Denio Barbosa Junior <deniojr@gmail.com>
2022-09-13 14:47:46 -0400

    Commit change

--- server.js
+++ server.js
@@ -1,3 +1,21 @@
-import lib from "lib";
-
-lib.PrintMsg("Denio");
+class Vehicle {
+    constructor(wheels) {
+        this.wheels = wheels;
+    }
+    toString() {
+        return '(' + this.wheels + ')';
+    }
+}
+class Car extends Vehicle {
+    constructor(color) {
+        super(4);
+        this.color = color;
+    }
+    toString() {
+        return super.toString() + ' colored: ' + this.color;
+    }
+}
+let car = new Car('blue');
+car.toString();
+if (car instanceof Car) console.log("car is an instance of Car");
+if (car instanceof Vehicle) console.log("car is an instance of Vehicle");
